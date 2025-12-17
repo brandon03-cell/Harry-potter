6f877b87cb613bec1d57d41f237c71d016c60efb
brandon03-cell <brandonchusgo2003@gmail.com>
2025-12-17 14:07:33 +0100

    auxilio

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 const form = document.getElementById('potter-form');
 const input = document.getElementById('potter-name');
 const potterInfoDiv = document.getElementById('potter-info');
+
+async function fetchPotterData(characterName) 
+{
+    try {
+        const response = await fetch(`https://potterhead-api.vercel.app/api/characters${characterName.toLowerCase()}`);
+
+        if (!response.ok) {throw new Error('Personaje no encontrado');}
+
+        const data = await response.json();
+        displayPotterInfo(data);
+    } catch (error) {
+        potterInfoDiv.innerHTML = `<p>${error.message}</p>`;
+        potterInfoDiv.style.display = 'block';
+    }
+}
+
+function displayPotterInfo(character) {
+    const {name, house, species, gender, ancestry, wand, patronus, image } = character;
+
+    potterInfoDiv.innerHTML = `
+        ${image ? `img src='${image}' alt='${name}'` : ''}
+        <h3>${name}</h3>
+        <p><strong>Casa:</strong> ${house || 'Desconocida'}</p>
+        <p><strong>Patronus:</strong> ${patronus || 'Desconocido'}<p/>
+    `;
+}
